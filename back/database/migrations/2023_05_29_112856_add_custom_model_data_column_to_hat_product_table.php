b7c7d8ce43e2ff4b7cc5d20796fc1b976be14e43
Gleb <M20XV@yandex.ru>
2023-05-29 14:32:32 +0300

    migration(back): add add_custom_model_data_column_to_hat_product_table migration

---
+++ back/database/migrations/2023_05_29_112856_add_custom_model_data_column_to_hat_product_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('hat_product', function (Blueprint $table) {
+            $table->unsignedInteger('custom_model_data');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('hat_product', function (Blueprint $table) {
+            $table->dropColumn('custom_model_data');
+        });
+    }
+};
