0e177a38393739d2f2c1883a92425691a9c4984c
Gleb <M20XV@yandex.ru>
2023-05-29 16:28:27 +0300

    feat(back/hat): implement crud

--- back/app/Repositories/HatProductRepository.php
+++ back/app/Repositories/HatProductRepository.php
@@ -2,7 +2,30 @@
 
 namespace App\Repositories;
 
+use App\DTO\HatProduct\CreateHatProductDTO;
+use App\DTO\HatProduct\UpdateHatProductDTO;
+use App\Models\HatProduct;
+
 class HatProductRepository
 {
+  function find(int $pageSize = 10, int $page = $_GET['page'] ?? 1)
+  {
+    $_GET['page'] = $page;
+    return HatProduct::paginate($pageSize);
+  }
 
+  function create(CreateHatProductDTO $dto): HatProduct
+  {
+    return HatProduct::create($dto);
+  }
+
+  function update(UpdateHatProductDTO $dto): HatProduct
+  {
+    return HatProduct::find('id', $dto->id);
+  }
+
+  function destroy(int $id): bool
+  {
+    return HatProduct::destroy($id) === 1;
+  }
 }
--- back/app/Services/HatService.php
+++ back/app/Services/HatService.php
@@ -2,12 +2,34 @@
 
 namespace App\Services;
 
+use App\DTO\HatProduct\CreateHatProductDTO;
+use App\DTO\HatProduct\UpdateHatProductDTO;
 use App\Repositories\HatProductRepository;
 
 class HatProductService
 {
   function __construct(
-    private HatProductRepository $repo
+    private HatProductRepository $productRepo
   ) {
+  }
+
+  function findProducts()
+  {
+    return $this->productRepo->find();
+  }
+
+  function createProduct(CreateHatProductDTO $dto)
+  {
+    return $this->productRepo->create($dto);
+  }
+
+  function updateProduct(UpdateHatProductDTO $dto)
+  {
+    return $this->productRepo->update($dto);
+  }
+
+  function destroy(int $id)
+  {
+    return $this->productRepo->destroy($id);
   }
 }
