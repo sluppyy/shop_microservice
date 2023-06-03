a2e41de0e3e5b7ea71c443f0628722c9bd3f99a9
Gleb <M20XV@yandex.ru>
2023-06-03 20:25:36 +0300

    refactor(back/admin_panel): use Paginator component

--- back/resources/js/Pages/HatPurchases/Index.vue
+++ back/resources/js/Pages/HatPurchases/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import Paginator from '@/Components/Paginator.vue'
 
 defineProps({ purchases: Object })
 </script>
@@ -46,20 +47,7 @@ defineProps({ purchases: Object })
             </tbody>
             </table>
 
-            <div class="flex flex-wrap justify-center mt-8" v-if="purchases.links.length > 3">
-                <template v-for="(link, k) in purchases.links" :key="k">
-                    <div v-if="link.url === null" 
-                        class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
-                        v-html="link.label" />
-
-                    <Link
-                        v-else
-                        class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded focus:border-indigo-500 focus:text-indigo-500"
-                        :class="{ 'bg-gray-900 text-white hover:bg-gray-800': link.active, 'hover:bg-gray-200': !link.active }"
-                        :href="link.url"
-                        v-html="link.label"/>
-                </template>
-            </div>
+            <Paginator :paginator="purchases"/>
         </div>
     </AuthenticatedLayout>
 </template>
